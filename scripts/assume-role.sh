e56b3a577422b82fd768c39c5b0fc4613ae3ec52
nszigeti <nszigeti@cmtelematics.com>
2024-05-24 16:17:25 +0200

    assume-role generates session IDs with 15 minutes expiration limit

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 source ../credentials/iam.sh
-aws sts assume-role --role-arn arn:aws:iam::211125513326:role/foglalo_publish_role --role-session-name foglalo_publish_role
+aws sts assume-role \
+  --role-arn arn:aws:iam::211125513326:role/foglalo_publish_role \
+  --role-session-name foglalo_publish_role \
+  --duration-seconds 900
