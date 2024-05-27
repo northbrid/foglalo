aa6d0052e9c14cee31bda3ea22d9f78a0d7da881
nszigeti <nszigeti@cmtelematics.com>
2024-05-27 12:28:38 +0200

    added email subscription to sns topic (as a backup for sms)

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -7,3 +7,9 @@ resource "aws_sns_topic_subscription" "foglalo_notification_sms_sub" {
   endpoint  = "+36706838528"
   protocol  = "sms"
 }
+
+resource "aws_sns_topic_subscription" "foglalo_notification_email_sub" {
+  topic_arn = aws_sns_topic.foglalo_notification_topic.arn
+  endpoint  = "northbrid@gmail.com"
+  protocol  = "email"
+}
